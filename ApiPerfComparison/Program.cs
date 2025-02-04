504f53b3e4b139a2ef245a107f26d240be4ea029
SPYROS\Spyros <sponaris@gmail.com>
2025-02-04 19:30:27 +0200

    .

--- ApiPerfComparison/Program.cs
+++ ApiPerfComparison/Program.cs
@@ -4,7 +4,39 @@ using ApiPerfComparison.Middleware;
 
 using FluentValidation;
 
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' followed by your token in the input field (e.g., 'Bearer your_token')."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
@@ -19,6 +51,12 @@ app.UseAuthentication();
 
 app.MapControllers();
 app.RegisterEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Register the middleware
 //app.UseMiddleware<ValidationMiddleware>();
