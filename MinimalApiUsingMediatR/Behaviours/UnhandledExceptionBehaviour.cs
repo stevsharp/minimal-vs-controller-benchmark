70dea5cc67868a618d965eab734986d56504f259
SPYROS\Spyros <sponaris@gmail.com>
2025-03-03 20:27:23 +0200

    Add Mediatr Behaviours

---
+++ MinimalApiUsingMediatR/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+public class UnhandledExceptionBehaviour<TRequest, TResponse>(ILogger<TRequest> logger) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<TRequest> _logger = logger;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception ex)
+        {
+            var requestName = typeof(TRequest).Name;
+
+            _logger.LogError(ex, "Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+
+            throw;
+        }
+    }
+}
